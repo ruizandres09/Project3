ebf52a9a153127d398df6f5827bd3186fd2401ac
Ana Pineda <anamichellepd@gmail.com>
2020-05-19 12:48:44 -0700

    added the first part of creating Auth0 authentication. Next I will call the API

--- models/log.js
+++ models/log.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const logSchema = new Schema({
-    date: {type:[String], required: true},
-    body: {type: [String], required: true},
+  date: { type: Date, default: Date.now },
+  body: { type: String, required: true },
 });
 
 const Log = mongoose.model("Log", logSchema);
